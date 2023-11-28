a8a233790835f1d0491ccd86ad33325b7c0d253a
Prajjwal_sharma <cpalgorithm.daily1@gmail.com>
2023-11-28 16:57:00 +0530

    new version change

---
+++ OOPS/DecodeString.java
@@ -0,0 +1,12 @@
+package OOPS;
+
+
+public class DecodeString {
+    public static void main(String[] args) {
+
+        System.out.println("Prajjwal sharma");
+
+        
+
+    }
+}
---
+++ OOPS/Helloworld.java
@@ -0,0 +1,15 @@
+ package OOPS;
+
+ public class Helloworld{
+ 
+public static void main(String [] args)
+{
+
+System.out.println("hello prajjwal sharma");
+
+
+}
+
+
+
+ }
